ca06a92a75b4795a736eb194b4c7ad0df990a9cd
Your Name <you@example.com>
2017-07-17 20:18:52 +0200

    r5v2

--- TEST2.php
+++ TEST2.php
@@ -6,5 +6,6 @@ echo" Invat GIT Test 2- modific acest fisier-modific nou"
 echo "Sunt pe ramura 2";
 echo "informatie noua";
 echo "alta info";
-echo "Modificare pe ramura 5"
+echo "Modificare pe ramura 5";
+echo "Modificare 2 pe ramura 5";
 ?>
